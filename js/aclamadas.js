da724667d34dd2bc86da27cf3ab13fd6356b1282
rik <ingeniero_rik@hotmail.com>
2024-05-30 23:18:56 -0300

    se usa array para aclamadas

--- js/aclamadas.js
+++ js/aclamadas.js
@@ -1,22 +1,11 @@
 
-const fotosAclamadas = [
-    { src: './assets/img/deep_purple_2.jpg', alt: 'aclamada_1', loading: 'deep_purple' },
-    { src: './assets/img/deep_purple_3.jpg', alt: 'aclamada_2', loading: 'deep_purple' },
-    { src: './assets/img/kiss_4.jpg', alt: 'aclamada_3', loading: 'kiss' },
-    { src: './assets/img/kiss_5.jpg', alt: 'aclamada_4', loading: 'kiss' },
-    { src: './assets/img/viticus_4.jpg', alt: 'aclamada_5', loading: 'kiss' },
-    { src: './assets/img/kiss_6.jpg', alt: 'aclamada_6', loading: 'kiss' },
-    { src: './assets/img/kiss_11.jpg', alt: 'aclamada_7', loading: 'kiss' },
-    { src: './assets/img/kiss_8.jpg', alt: 'aclamada_8', loading: 'kiss' },
-    { src: './assets/img/kiss_9.jpg', alt: 'aclamada_9', loading: 'kiss' },
-    { src: './assets/img/kiss_10.jpg', alt: 'aclamada_10', loading: 'kiss' },
-    { src: './assets/img/kiss_11.jpg', alt: 'aclamada_11', loading: 'kiss' },
-    { src: './assets/img/scorpions_3.jpg', alt: 'aclamada_12', loading: 'kiss' },
-];
+// agregar en el index.html type="module" en el script que llama a este js
+import {fotosAclamadas} from "./data.js";
 
 function generarFotosAclamadas(fotos) {
-    const seccionFotos = document.querySelector('.aclamadas');
 
+    
+    const seccionFotos = document.querySelector('.fotosAclamadas');
     
     const divAclamadas = document.createElement('div');
     divAclamadas.classList.add('aclamadas');
---
+++ js/data.js
@@ -0,0 +1,17 @@
+export const fotosAclamadas = [
+    { src: './assets/img/deep_purple_2.jpg', alt: 'aclamada_1', loading: 'deep_purple' },
+    { src: './assets/img/deep_purple_3.jpg', alt: 'aclamada_2', loading: 'deep_purple' },
+    { src: './assets/img/kiss_4.jpg', alt: 'aclamada_3', loading: 'kiss' },
+    { src: './assets/img/kiss_5.jpg', alt: 'aclamada_4', loading: 'kiss' },
+    { src: './assets/img/viticus_4.jpg', alt: 'aclamada_5', loading: 'kiss' },
+    { src: './assets/img/kiss_6.jpg', alt: 'aclamada_6', loading: 'kiss' },
+    { src: './assets/img/kiss_11.jpg', alt: 'aclamada_7', loading: 'kiss' },
+    { src: './assets/img/kiss_8.jpg', alt: 'aclamada_8', loading: 'kiss' },
+    { src: './assets/img/kiss_9.jpg', alt: 'aclamada_9', loading: 'kiss' },
+    { src: './assets/img/kiss_10.jpg', alt: 'aclamada_10', loading: 'kiss' },
+    { src: './assets/img/kiss_11.jpg', alt: 'aclamada_11', loading: 'kiss' },
+    { src: './assets/img/scorpions_3.jpg', alt: 'aclamada_12', loading: 'kiss' },
+];
+
+
+//module.exports = fotosAclamadas;
